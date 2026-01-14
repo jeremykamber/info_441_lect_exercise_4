763f8af8480e461efaadbd188b3005826189ef60
Slys <slysdeveloper@gmail.com>
2026-01-14 10:22:01 -0800

    fix: fix incorrect bored api url

--- routes/activities.js
+++ routes/activities.js
@@ -1,12 +1,12 @@
 import express from "express";
 var router = express.Router();
 
-const apiUrl = "https://www.boredapi.com/api/activity";
+const apiUrl = "https://bored-api.appbrewery.com/random";
 
 router.get("/getRandom", async (req, res, next) => {
     const response = await fetch(apiUrl);
     const responseJson = await response.json();
-    res.send(responseJson);
+    res.send(responseJson.activity);
 });
 
 export default router;
